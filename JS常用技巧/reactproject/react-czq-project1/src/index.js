20c696b52a3d7f760876a609d5c3750c85fdc9ac
chengzq <1191046010@qq.com>
2020-03-28 02:16:51 +0800

    期盼游戏提交

--- JS常用技巧/reactproject/react-czq-project1/src/index.js
+++ JS常用技巧/reactproject/react-czq-project1/src/index.js
@@ -4,57 +4,78 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-
 //父组件
 class Board extends React.Component {
-  renderSquare(i) {
-    return <Square value={i} />;
+  constructor(props) {
+    super(props)
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true
+    }
   }
+
+  renderSquare(i) {
+    return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)} />;
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O'
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext
+    });
+  }
+
   render() {
-    const status = 'Next player: X';
+    // const status = 'Next player:'+(this.state.xIsNext ? '张三-X':'李四-O');
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    status = winner ? 'Winner: ' + winner : 'Next player: ' + (this.state.xIsNext ? '张三-X' : '李四-O');
     return (
       <div>
         <div className='status'>{status}</div>
         <div className='board-row'>
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
+          {this.renderSquare(0)}{this.renderSquare(1)}{this.renderSquare(2)}
         </div>
         <div className='board-row'>
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
+          {this.renderSquare(3)}{this.renderSquare(4)}{this.renderSquare(5)}
         </div>
         <div className='board-row'>
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
+          {this.renderSquare(6)}{this.renderSquare(7)}{this.renderSquare(8)}
         </div>
       </div>
     );
   }
 }
 
-// //子组件 生成可点击的棋盘按钮
-class Square extends React.Component {
-  // 在 JavaScript class 中，每次你定义其子类的构造函数时，都需要调用 super 方法。
-  constructor(props) {
-    super(props)//在所有含有构造函数的的 React 组件中，构造函数必须以 super(props) 开头。
-    this.state = {
-      value: null
-    }
-  }
-  render() {
-    return (
-      <button
-        className='square'
-        onClick={() => { this.setState({ value: 'X' }) }}
-      >
-        {/* {this.props.value} */this.state.value}
-      </button>
-    );
-  }
+//子组件 生成可点击的棋盘按钮
+// class Square extends React.Component {
+// 在 JavaScript class 中，每次你定义其子类的构造函数时，都需要调用 super 方法。
+// constructor(props) {
+//   super(props)//在所有含有构造函数的的 React 组件中，构造函数必须以 super(props) 开头。
+//   this.state = {
+//     value: null
+//   }
+// }
+//将Square 更改为函数组件
+function Square(props) {
+  return (
+    <button
+      className='square'
+      onClick={props.onClick}
+    >
+      {props.value}
+    </button>
+  );
 }
+
+// }
+
+
 
 
 //井字琪练习
@@ -74,6 +95,25 @@ class Game extends React.Component {
   }
 }
 
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
 
 ReactDOM.render(
   // <React.StrictMode>
